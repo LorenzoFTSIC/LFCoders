0df26d39d9fce92a1c636bb14e503d92b5226be2
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-31 12:46:12 -0400

    added Looker!

--- client/src/components/SmallHeader/index.js
+++ client/src/components/SmallHeader/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../assets/img/Looker-Placeholder.png';
+import Looker from '../../assets/img/Looker-Placeholder.png';
 
 const style = {
   // The underline on the site initials won't come off... In devtools it says its attributed to a:-webkit-any-link (user agent stylesheet)
@@ -13,15 +15,9 @@ const Header = () => {
     <header className="bg-info text-dark mb-4 py-3">
       <div className="d-inline-flex justify-space-between mx-3">
         <Link className="text-dark logo" to="/" style={style.a}>
-          <div className="d-inline-flex justify-space-between">
-            <img
-              className="looker mx-3"
-              src="../../assets/img/Looker-Placeholder.png"
-              alt="Looker"
-            ></img>
-            <h1 className="mx-3">
-              LFC
-            </h1>
+          <div className="d-inline-flex justify-space-between align-items-end">
+            <img className="looker mx-3" src={Looker} alt="Looker"></img>
+            <h1 className="mx-3">LFC</h1>
           </div>
         </Link>
       </div>
