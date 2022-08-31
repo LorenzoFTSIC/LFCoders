738fd92726028cf93465000c5206b46fc4155c45
LorenzoF <lorenzoferritsic@gmail.com>
2022-08-31 13:09:03 -0400

    Added to models and added comments to confusing parts

--- server/models/Project.js
+++ server/models/Project.js
@@ -9,15 +9,25 @@ const projectSchema = new Schema({
   description: {
     type: String,
   },
-  stackType: {
-    type: String,
-  },
-  skills: {
-    type: String,
-  },
+  // references the skill model to have all the skills used on this project
+  skills: [
+    {
+        type: String,
+    },
+  ],
+  // references profile to get all the users collaborating on this project
   profile: {
     type: String,
+    required: true,
   },
+  createData: {
+    type: Date,
+    default: Date.now
+  },
+  // true/false to see if the project is marked "done" or not
+  completed: {
+    type: Boolean
+  }
 });
 
 const Project = model('Project', projectSchema);
--- server/models/Skill.js
+++ server/models/Skill.js
@@ -1,11 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const skillSchema = new Schema({
+  // name of the skill ( ex bootstrap, node, mongodb, etc)
   name: { 
     type: String,
     required: true,
     unique: true,
   },
+  // referring to where the skill is applied to (ex bootstrap would have frontend, mongodb would have backend, etc)
   stackType: {
     type: String,
     required: true,
