0609f6a74a4bda4b0ad33f04a2ad89677895b8b4
kmart134 <ksmgps@hotmail.com>
2022-08-31 13:56:32 -0400

    go back button styled

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+const styles = {
+  button: {
+    backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
+    color: 'white'
+  }
+}
+
 const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -9,13 +16,14 @@ const Footer = () => {
       <div className="container text-center mb-5">
         {location.pathname !== '/' && (
           <button
-            className="btn btn-dark mb-3"
+            style={styles.button}
+            className="btn mb-3"
             onClick={() => navigate(-1)}
           >
             &larr; Go Back
           </button>
         )}
-        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+        <h4>&copy; {new Date().getFullYear()} - Git Buddies</h4>
       </div>
     </footer>
   );
