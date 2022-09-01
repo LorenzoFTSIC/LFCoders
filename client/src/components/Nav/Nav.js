4bef88f7cae17c4b807daa395b2db29872d58047
kmart134 <ksmgps@hotmail.com>
2022-09-01 13:22:40 -0400

    navtab buttons style and navtab text targeted

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,52 +3,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-// const styles = {
-//   navbarStyle: {
-//     background: 'pink',
-//     justifyContent: 'flex-end',
-//   },
-// };
+const styles = {
+    navtab: {
+        backgroundColor: '#a4b5f3',
+        marginLeft: '20px'
+      },
+      navtabtext: {
+        color: 'white',
+        fontWeight: 'bold'
+      }
+};
 
 function Nav({currentPage, handlePageChange}) {
 
     return(
         <nav>
         <ul className="nav nav-tabs">
-        <li>
-        <Link to=''
+        <li style={styles.navtab} >
+        <Link style={styles.navtabtext} to=''
         onClick={() => handlePageChange('Recent Collaborations')}
         className={currentPage === 'Recent Collaborations' ? 'nav-active nav-link' : 'nav-link'}>
         Recent Collaborations
         </Link>
         </li>
 
-        <li className='nav-item'>
-        <Link to='/search'
+        <li style={styles.navtab} className='nav-item'>
+        <Link style={styles.navtabtext} to='/search'
         onClick={() => handlePageChange('Search')}
         className={currentPage === 'Search' ? 'nav-active nav-link' : 'nav-link'}>
         Search
         </Link>
         </li>
 
-        <li className='nav-item'>
-        <Link to=''
+        <li style={styles.navtab} className='nav-item'>
+        <Link style={styles.navtabtext} to=''
         onClick={() => handlePageChange('Messages')}
         className={currentPage === 'Messages' ? 'nav-active nav-link' : 'nav-link'}>
         Messages
         </Link>
         </li>
 
-        <li className='nav-item'>
-        <Link to='/me'
+        <li style={styles.navtab} className='nav-item'>
+        <Link style={styles.navtabtext} to='/me'
         onClick={() => handlePageChange('Profile')}
         className={currentPage === 'Profile' ? 'nav-active nav-link' : 'nav-link'}>
         Profile
         </Link>
         </li>
 
-        <li className='nav-item'>
-        <Link to='/'
+        <li style={styles.navtab} className='nav-item'>
+        <Link style={styles.navtabtext} to='/'
         onClick={() => handlePageChange('Logout')}
         className={currentPage === 'Logout' ? 'nav-active nav-link' : 'nav-link'}>
         Logout
