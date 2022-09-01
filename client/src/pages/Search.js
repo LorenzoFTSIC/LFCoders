42e92f2cd54b8cd0b180ab8387416ecc8e6dd0aa
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-01 09:11:08 -0400

    added styles to tab box within the component and in the index.css (because of jsx restrictions). Changed the background color of the body element to #161616 so that it wasn't totally pitch black.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,31 +3,81 @@ import React from 'react';
 import LFCForm from '../components/LFCForm';
 import LTCForm from '../components/LTCForm';
 
+const style = {
+  container: {
+    maxWidth: '40%',
+    margin: '0 auto',
+    padding: 20,
+    background: '#333',
+    color: 'whitesmoke',
+    fontSize: 18
+  },
+  tabs: {
+    minHeight: 300,
+    overflow: 'hidden',
+    borderRadius: '8px 8px 0 0',
+    position: 'relative',
+    display: 'flex'
+  },
+  tab: {
+    flex: 1
+  },
+  tabLabel: {
+    height: 40,
+    padding: 10,
+    background: '#9768D1',
+    textAlign: 'center',
+    cursor: 'pointer',
+    display: 'block',
+    transition: 'background 0.5s ease'
+  },
+  tabContent: {
+    padding: 20,
+    background: '#553285',
+    borderRadius: '0 0 8px 8px',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    right: 0,
+    top: 40,
+    transition: 'opacity 0.8s ease, transform 0.8s ease',
+    // show/hide
+    opacity: 0,
+    transform: 'scale(0.1)',
+    transformOrigin: 'top left'
+  }
+};
+
 const Search = () => {
   return (
     <div className="searchPage">
-      <div class="container">
+      <div style={style.container}>
         <h1>Search</h1>
 
-        <div class="tabs">
-          <div class="tab">
+        <div style={style.tabs} className='tabs'>
+          <div style={style.tab} className='tab'>
             <input type="radio" id="tab-1" name="tabs" checked />
-            <label for="tab-1">Looking For Coders</label>
-            <div class="tab-content">
+            <label for="tab-1" style={style.tabLabel}>
+              Looking For Coders
+            </label>
+            <div style={style.tabContent} className='tab-content'>
               <LFCForm />
             </div>
           </div>
 
-          <div class="tab">
+          <div style={style.tab} className='tab'>
             <input type="radio" id="tab-2" name="tabs" />
-            <label for="tab-2">Looking To Code</label>
-            <div class="tab-content">
+            <label for="tab-2" style={style.tabLabel}>
+              Looking To Code
+            </label>
+            <div style={style.tabContent} className='tab-content'>
               <LTCForm />
             </div>
           </div>
         </div>
       </div>
     </div>
+    //   HTML and CSS by AlvaroTrigo on CodePen: TabbyTabs
   );
 };
 
