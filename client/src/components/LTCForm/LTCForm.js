185f5c533d9ac4f9114d6903e548d1eb5eb7d0a9
kmart134 <ksmgps@hotmail.com>
2022-09-02 07:22:07 -0400

    LTCForm created and exporting

---
+++ client/src/components/LTCForm/LTCForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+// const styles = {
+//   nav: {
+//     display: 'flex',
+//     alignItems: 'flex-end'
+//   }
+// };
+
+const LTCForm = () => {
+  return (
+    <header className="bg-info text-dark mb-4 py-3">
+   LOOKING TO CODE
+  </header>
+  );
+}
+
+
+
+export default LTCForm;
