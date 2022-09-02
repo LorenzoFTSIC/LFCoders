4c0fe882b6b30de3e9705a328dcaf49db3d04f65
kmart134 <ksmgps@hotmail.com>
2022-09-02 14:52:37 -0400

    tiny robot added to small header.js

--- client/src/components/SmallHeader/index.js
+++ client/src/components/SmallHeader/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import '../../assets/img/Looker-Placeholder.png';
-import Looker from '../../assets/img/Looker-Placeholder.png';
+import Looker from '../../assets/img/welcomerobot.png';
 
 const style = {
   // The underline on the site initials won't come off... In devtools it says its attributed to a:-webkit-any-link (user agent stylesheet)
   a: {
     textUnderline: 'none !important'
   },
-  
+
   big: {
     backgroundImage:
       'linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))',
@@ -18,7 +18,9 @@ const style = {
   header: {
     width: '100%',
     justifyContent: 'space-between',
-    
+  },
+  looker: {
+    height: "120px"
   }
 };
 
@@ -28,7 +30,7 @@ const SmallHeader = () => {
       <div className="d-inline-flex" style={style.header}>
         <Link className="text-dark logo" to="/" style={style.a}>
           <div className="d-inline-flex justify-space-between align-items-end">
-            <img className="looker mx-2" src={Looker} alt="Looker"></img>
+            <img className="looker mx-2" src={Looker} alt="Looker" style={style.looker}></img>
             <h1 className="mx-2">LFC</h1>
           </div>
         </Link>
