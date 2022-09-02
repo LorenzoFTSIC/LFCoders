9efa10d578b988d6d6bc95b496c4bfb31de149e6
khaledghanem1 <khaledghanim96@gmail.com>
2022-09-02 09:20:34 -0500

    Fixing signup/login error

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -154,14 +154,14 @@ const Header = ({ reasons }) => {
                 <Link
                   style={styles.button}
                   className="btn btn-lg btn-light m-2"
-                  to="/login"
+                  to="/login" data-toggle="modal" data-target=".bd-example-modal-lg-login"
                 >
                   Login
                 </Link>
                 <Link
                   style={styles.button}
                   className="btn btn-lg btn-light m-2"
-                  to="/signup"
+                  to="/signup" data-toggle="modal" data-target=".bd-example-modal-lg-signup"
                 >
                   Signup
                 </Link>
