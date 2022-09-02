6fc124538a3f961f5b531c2070ee0e5f04ff0ade
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-01 23:26:54 -0400

    Add more content to header, have it sit side by side and adjust styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,32 +5,34 @@ import Auth from '../../utils/auth';
 
 const styles = {
   header: {
-    height:'400px',
-    margin: 20,
-    backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
-    textAlign: 'center',
+    minHeight: '350px',
+    backgroundImage:
+      'linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))',
+    textAlign: 'center'
   },
   headerContent: {
-    color:'white',
-    paddingTop:'90px'
+    padding: 0, 
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  headerSides: { 
+    width: '50%'
+  },
+  title: {
+    color: 'white',
+    fontSize: '3rem',
+    fontFamily: 'Segoe UI'
   },
 
-  title: {
-    color:'white',
-    paddingTop:'30px',
-    fontSize: '3rem',
-    fontFamily: 'Segoe UI',
-  },
-  
   link: {
-    textDecoration: 'none',
+    textDecoration: 'none'
   },
 
   button: {
     backgroundColor: '#a4b5f3',
     color: '#58497b'
   }
-  
 };
 
 const Header = () => {
@@ -39,36 +41,64 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header style={styles.header} className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div style={styles.headerContent} className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link style={styles.link} className="none" to="/" >
-          <h1  style={styles.title} className="m-0">
+    <header
+      style={styles.header}
+      className="bg-info text-dark display-flex align-center"
+    >
+      <div
+        style={styles.headerContent}
+        className="container"
+      >
+        <div style={styles.headerSides}>
+          {/* <Link style={styles.link} className="none" to="/"> */}
+          <h1 style={styles.title} className="m-0">
             L.F.Coders
           </h1>
-        </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
-        </p>
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/me">
-                View My Profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link style={styles.button} className="btn btn-lg btn-light m-2" to="/login">
-                Login
-              </Link>
-              <Link style={styles.button} className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
+          {/* </Link> */}
+          <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+            Ideas that go beyond our current knowledge. <br /> Working in a
+            team. <br />
+            Spreading the work load. <br />
+            Bouncing ideas. <br />
+            Meeting new people. <br />
+            Sharing ideas.
+          </p>
+        </div>
+
+        <div style={styles.headerSides}>
+          <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+            Whatever your reason, LFC is here to get you in touch with the right
+            people for your next coding project.
+          </p>
+          <div>
+            {Auth.loggedIn() ? (
+              <>
+                <Link className="btn btn-lg btn-primary m-2" to="/me">
+                  View My Profile
+                </Link>
+                <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  style={styles.button}
+                  className="btn btn-lg btn-light m-2"
+                  to="/login"
+                >
+                  Login
+                </Link>
+                <Link
+                  style={styles.button}
+                  className="btn btn-lg btn-light m-2"
+                  to="/signup"
+                >
+                  Signup
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </header>
