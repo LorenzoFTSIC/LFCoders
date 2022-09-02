744e5004384bdc50cf78d7eeac58facd12f13a32
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-02 18:46:49 -0400

    added the commented out option of bringing in the QUERYs for bio (from queries.js and mutations.js in utils). Would also need to add a useQuery for profile (like in the Profile.js)

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 
+// import { useQuery, useMutation } from '@apollo/client';
+// import { QUERY_BIO } from '../utils/queries';
+// import { EDIT_BIO } from '../utils/mutations';
+
 const Settings = () => {
-  const [textarea, setTextarea] = useState('{profile.bio}');
-  // ^ I'm hoping this variable will bring in whatever the poster originally put as a Bio 
+  const [textarea, setTextarea] = useState('');
 
   const handleChange = (e) => {
     setTextarea(e.target.value);
   };
 
+    //   let { id } = useParams();
+
+  // const { loading, data } = useQuery(QUERY_BIO, {
+  //   variables: { _id: id },
+  // });
+
+  // QUERY PROFILE NOT THE QUERY_BIO *** 
+
+  // const profile = data?.profile || [];
+
+  // const [editBio, { error }] = useMutation(EDIT_BIO);
+
+  // const handleBio = async (bio) => {
+  //   try {
+  //     await editBio({
+  //       variables: { _id: id, bio: bio },
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  
+  // PUTTING SETTINGS AS A MODAL SO THAT THE INFORMATION COULD THEN BE BROUGHT IN AS A PROP 
   return (
     <>
       <h2>Settings</h2>
