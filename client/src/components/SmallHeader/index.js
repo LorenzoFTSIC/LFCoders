5e45654e836969797adc88a7f113e174c61ae8a6
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-03 10:24:56 -0400

    deleted the a styling in the smallHeader component becabecause it's fixed in the css; fixed the header spacing a bit but it still needs work. Added a purple background color.

--- client/src/components/SmallHeader/index.js
+++ client/src/components/SmallHeader/index.js
@@ -5,11 +5,13 @@ import '../../assets/img/Looker-Placeholder.png';
 import Looker from '../../assets/img/Looker-Placeholder.png';
 
 const style = {
-  // The underline on the site initials won't come off... In devtools it says its attributed to a:-webkit-any-link (user agent stylesheet)
-  a: {
-    textUnderline: 'none !important'
-  },
   header: {
+    width: '100%',
+    padding: '10px 30px',
+    background: '#793285',
+    justifyContent: 'space-between', 
+  }, 
+  headerSections: { 
     width: '100%',
     justifyContent: 'space-between'
   }
@@ -17,12 +19,12 @@ const style = {
 
 const SmallHeader = () => {
   return (
-    <header className="bg-info text-dark mb-4 py-3">
-      <div className="d-inline-flex" style={style.header}>
-        <Link className="text-dark logo" to="/" style={style.a}>
-          <div className="d-inline-flex justify-space-between align-items-end">
-            <img className="looker mx-2" src={Looker} alt="Looker"></img>
-            <h1 className="mx-2">LFC</h1>
+    <header style={style.header}>
+      <div className="d-inline-flex" style={style.headerSections}>
+        <Link className="text-dark logo" to="/" >
+          <div className="d-inline-flex align-items-end">
+            <img className="looker" src={Looker} alt="Looker"></img>
+            <h1 className="m-3">LFC</h1>
           </div>
         </Link>
         <Nav/>
