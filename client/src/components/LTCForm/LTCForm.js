76aa9e8fc897427bc5073774a0a0d73d9936b205
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-03 15:51:31 -0400

    gave classNames to parts of the modal to start making a container to use in every modal.

--- client/src/components/LTCForm/LTCForm.js
+++ client/src/components/LTCForm/LTCForm.js
@@ -1,62 +1,62 @@
-import React from 'react';
+// import React from 'react';
 
 
 
-// const styles = {
-//   nav: {
-//     display: 'flex',
-//     alignItems: 'flex-end'
-//   }
-// };
+// // const styles = {
+// //   nav: {
+// //     display: 'flex',
+// //     alignItems: 'flex-end'
+// //   }
+// // };
 
-const LTCForm = () => {
-    // const handleChange = (event) => {
-    //     event.preventDefault();
-    //     const { name, value } = event.target;
-    // };
-  return (
-    <div>
-      <form>
-         LOOKING TO CODE
-       <input         
-        className="form-input"
-        placeholder="Your username"
-        name="name"
-        type="text"
+// const LTCForm = () => {
+//     // const handleChange = (event) => {
+//     //     event.preventDefault();
+//     //     const { name, value } = event.target;
+//     // };
+//   return (
+//     <div>
+//       <form>
+//          LOOKING TO CODE
+//        <input         
+//         className="form-input"
+//         placeholder="Your username"
+//         name="name"
+//         type="text"
                           
-        // onChange={handleChange}
-        />
-        <input
+//         // onChange={handleChange}
+//         />
+//         <input
                           
-        className="form-input"
-        placeholder="Your email"
-        name="email"
-        type="email"
+//         className="form-input"
+//         placeholder="Your email"
+//         name="email"
+//         type="email"
                           
-        // onChange={handleChange}
-        />
-        <input
+//         // onChange={handleChange}
+//         />
+//         <input
                          
-        className="form-input"
-        placeholder="******"
-        name="password"
-        type="password"
+//         className="form-input"
+//         placeholder="******"
+//         name="password"
+//         type="password"
                          
-        // onChange={handleChange}
-         />
+//         // onChange={handleChange}
+//          />
                         
-        <button
-        className="btn btn-block btn-info"
-        type="submit"
-        aria-label="Close"
-        >
-        Submit
-         </button>
-    </form>
-    </div>
-  );
-}
+//         <button
+//         className="btn btn-block btn-info"
+//         type="submit"
+//         aria-label="Close"
+//         >
+//         Submit
+//          </button>
+//     </form>
+//     </div>
+//   );
+// }
 
 
 
-export default LTCForm;
+// export default LTCForm;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -11,136 +11,175 @@ const style = {
     background: '#333',
     color: 'whitesmoke',
     fontSize: 18,
-    borderRadius: "12px"
-
+    borderRadius: '12px'
   },
 
   maintitle: {
     fontSize: 40,
-    textAlign: "center",
-    color: "white",
-    marginTop:"10px",
-    marginBottom:"40px"
+    textAlign: 'center',
+    color: 'white',
+    marginTop: '10px',
+    marginBottom: '40px'
   },
 
   title: {
     fontSize: 30,
-    textAlign: "center",
-    fontWeight:"bold",
-    color: "white",
-    marginTop:"10px",
-    marginBottom:"40px",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: '10px',
+    marginBottom: '40px'
   },
 
   heading3: {
     fontSize: 25,
-    marginTop:"20px"
-
+    marginTop: '20px'
   },
-  
+
   button: {
     fontSize: 15,
-    padding:"10px",
-    color: "black",
+    padding: '10px',
+    color: 'black',
     fontWeight: 'bold',
     backgroundColor: '#d6d1d1',
-    marginLeft:"3px",
-    marginBottom:"4px"
-  },
-  
+    marginLeft: '3px',
+    marginBottom: '4px'
+  }
 };
 
 const Search = () => {
   return (
-    <div> 
-    <h1 className= "maintitle" style={style.maintitle}>SEARCH FOR PROJECTS</h1>
-    <div className="searchPage">
-      <div style={style.container}>
-        <h1 className= "title" style={style.title}>Looking to Code</h1>
-      <div>
+    <div className="modalPage">
+      <h1 className="mainTitle">LOOKING TO CODE</h1>
+      <div className="modalContainer">
+        {/* <div style={style.container}>
+        <h1 className= "title" style={style.title}></h1>
+      <div> */}
 
-      <form>
-        <h3 className="heading3" style={style.heading3}>What I bring to the table:</h3>
-         <div className= "">
-         <button className="button favorite styled" style={style.button}
-          type="radio">
-          Front End
-        </button>
+        <form>
+          <h3 className="sectionHeading" style={style.heading3}>
+            What I bring to the table:
+          </h3>
+          <div className="btnContainer">
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Front End
+            </button>
 
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Back End
-        </button>
-        </div>
-
-        <h3 className="heading3" style={style.heading3}>Expected Timeline:</h3>
-         <div className= "">
-         <button className="button favorite styled" style={style.button}
-          type="radio">
-          5 Hours
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          20 Hours
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          40+ Hours
-        </button>
-        </div>
-
-        <h3 className="heading3" style={style.heading3}>I am available...</h3>
-         <div className= "">
-         <button className="button favorite styled" style={style.button}
-          type="button">
-          Monday
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="button">
-          Tuesday
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Wednesday
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Thursday
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Friday
-        </button>
-
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Saturday
-        </button>
-        
-        <button className="button favorite styled" style={style.button}
-          type="radio">
-          Sunday
-        </button>
-        </div>
-                        
-        <button
-        className="btn btn-block btn-info"
-        type="submit"
-        aria-label="Close"
-        >
-        Submit
-         </button>
-    </form>
-    </div>
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Back End
+            </button>
           </div>
-        </div>
-        </div>
+
+          <h3 className="sectionHeading" style={style.heading3}>
+            Expected Timeline:
+          </h3>
+          <div className="btnContainer">
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              5 Hours
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              20 Hours
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              40+ Hours
+            </button>
+          </div>
+
+          <h3 className="sectionHeading" style={style.heading3}>
+            I am available...
+          </h3>
+          <div className="btnContainer">
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="button"
+            >
+              Monday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="button"
+            >
+              Tuesday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Wednesday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Thursday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Friday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Saturday
+            </button>
+
+            <button
+              className="button favorite styled"
+              style={style.button}
+              type="radio"
+            >
+              Sunday
+            </button>
+          </div>
+
+          <button
+            className="btn btn-block btn-info"
+            type="submit"
+            aria-label="Close"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+    // </div>
+    // </div>
     //   HTML and CSS by AlvaroTrigo on CodePen: TabbyTabs
   );
 };
