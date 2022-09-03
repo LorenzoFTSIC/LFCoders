d2bf3734393241e8132067f20e35b4b73d905be6
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-03 18:07:39 -0400

    edited the btn class using the go back button

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const styles = {
-  button: {
-    backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
-    color: 'white',
-  },
-  trademark: {
-    opacity: 0.2
-  }
-}
+// const styles = {
+//   // button: {
+//   //   backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
+//   //   color: 'white',
+//   // },
+//   trademark: {
+//     opacity: 0.2
+//   }
+// }
 
 const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
+    <footer>
+      <div className="container text-center">
         {location.pathname !== '/' && (
           <button
-            style={styles.button}
-            className="btn mb-3"
+            className="btn"
             onClick={() => navigate(-1)}
           >
             &larr; Go Back
           </button>
         )}
-        <h4 style={styles.trademark} >&copy; {new Date().getFullYear()} - Git Buddies</h4>
+        {/* <h4 style={styles.trademark} >&copy; {new Date().getFullYear()} - Git Buddies</h4> */}
       </div>
     </footer>
   );
