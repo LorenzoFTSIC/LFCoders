8dc6a96b229dc9f009874b91c2f8761bc8430ac8
kmart134 <ksmgps@hotmail.com>
2022-09-05 19:56:26 -0400

    readded contact us to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
+// import Contact from './pages/Contact';
 
-// const styles = {
-//   // button: {
-//   //   backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
-//   //   color: 'white',
-//   // },
-//   trademark: {
-//     opacity: 0.2
-//   }
-// }
+const styles = {
+  // button: {
+  //   backgroundImage:"linear-gradient(to right, rgb(52, 9, 138) , rgb(236, 185, 236))",
+  //   color: 'white',
+  // },
+  trademark: {
+    opacity: 0.2
+  }
+}
 const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -25,7 +26,13 @@ const Footer = () => {
             &larr; Go Back
           </button>
         )}
-        {/* <h4 style={styles.trademark} >&copy; {new Date().getFullYear()} - Git Buddies</h4> */}
+         <h4 style={styles.trademark} >
+        <Link to='/contact'
+        onClick={() => navigate('Contact')}>
+        Contact Us
+        </Link>
+        </h4>
+        <h4 style={styles.trademark} >&copy; {new Date().getFullYear()} - Git Buddies</h4>
       </div>
     </footer>
   );
