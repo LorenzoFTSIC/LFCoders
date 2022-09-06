348b3f4fc350e04704dc9a8568927091392ca2de
LorenzoF <lorenzoferritsic@gmail.com>
2022-09-06 12:18:43 -0400

    Fixed search page to accommodate what databse stores and did basic styling

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Button from '@mui/material/button';
+import Modal from '@mui/material/Modal';
+import { isNonEmptyArray } from '@apollo/client/utilities';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 
 // import LFCForm from '../components/LFCForm';
 // import LTCForm from '../components/LTCForm/LTCForm';
@@ -11,19 +19,76 @@ const style = {
     border: '1px solid whitesmoke',
     borderRadius: 'none',
     color: '#161616',
-  },
+  }
 };
 
 const Search = () => {
+
+
+  const [searchVisi, setSearchVisi] = useState(true);
+
+  const [formState, setFormState] = useState({
+    errors: {},
+    // profileId: '',
+    bio: ''
+  });
+
+  const handleChange = (e) => {
+    
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formState);
+  
+    setSearchVisi(current => !current)
+
+    // try {
+    //   const { Modeldata } = await editBio({
+    //     variables: { ...formState, profileId: profile._id }
+    //   });
+  
+    // } catch (e) {
+    //   console.error(e);
+    // }
+  };
+  
+  const skills = [
+    { label: "GraphQL" },
+    { label: "React" },
+    { label: "Angular" },
+    { label: "Apollo" },
+    { label: "HTML" },
+    { label: "CSS" },
+    { label: "SQL" },
+    { label: "MySQL" },
+    { label: "JQuery" },
+    { label: "Javascript" },
+    { label: "Bootstrap" },
+    { label: "Tailwind" },
+    { label: "Bulma" },
+    { label: "MaterialUI" },
+    { label: "Heroku" },
+    { label: "AWS" }
+
+  ]
+
+
   return (
-    <div className="modalPage">
+    <div className="modalPage" 
+    style={{display: searchVisi ? "flex" : "none" }}>
       <div className="modalContainer">
         {/* <div style={style.container}>
         <h1 className= "title" style={style.title}></h1>
       <div> */}
         <h3 className="mainTitle">LOOKING TO CODE</h3>
         <form>
-          <h3 className="sectionHeading">What I bring to the table:</h3>
+          {/* <h3 className="sectionHeading">What I bring to the table:</h3>
           <div className="buttonContainer">
             <button
               className="formOptionButton"
@@ -46,9 +111,23 @@ const Search = () => {
             >
               Back End
             </button>
-          </div>
+          </div> */}
 
-          <h3 className="sectionHeading">Expected Timeline:</h3>
+          <h3 className="sectionHeading">Project Name:</h3>
+          <TextField id="outlined-basic" 
+          label="Outlined" 
+          variant="outlined"
+          style={style.Button} />
+
+          <h3 className="sectionHeading">Skills Used:</h3>
+          <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={skills}
+          style={style.Button}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Skills" />} />
+          {/* <h3 className="sectionHeading">Expected Timeline:</h3>
           <div className="buttonContainer">
             <button
               className="formOptionButton"
@@ -81,10 +160,17 @@ const Search = () => {
               // type="radio"
             >
               40+ Hours
-            </button>
-          </div>
-
-          <h3 className="sectionHeading">I am available...</h3>
+            </button> */}
+          {/* </div> */}
+          <h3 className="sectionHeading">Completion Status:</h3>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group">
+            <FormControlLabel value="true" control={<Radio />} label="Completed" />
+            <FormControlLabel value="false" control={<Radio />} label="In Progress" />
+          </RadioGroup>
+          {/* <h3 className="sectionHeading">I am available...</h3>
           <div className="buttonContainer">
             <button
               className="formOptionButton"
@@ -162,12 +248,13 @@ const Search = () => {
             >
               Sunday
             </button>
-          </div>
+          </div> */}
           <button
             className="btn btn-block submit"
             type="submit"
             aria-label="Close"
             // style={style.submit}
+            onClick={handleFormSubmit}
           >
             Submit
           </button>
