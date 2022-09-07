fe24e741ab37088cbaea35620970081256e5303e
kmart134 <ksmgps@hotmail.com>
2022-09-06 21:00:19 -0400

    api functionality added but not working

--- client/src/components/Repos/index.js
+++ client/src/components/Repos/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import search from '../../utils/API';
+import { useState, useEffect } from 'react';
 
-const searchRepo = function(event){
-    event.preventDefault();
-}
+
 
 const Repos = () => {
+
+    const [result, setResults] = useState([]);
+
+
+    const searchRepo = async (query) => {
+        const response = await search(query);
+        setResults(response.data.data);
+      };
+
+    useEffect(() => {
+    searchRepo(`githubusername`);
+    //profile.github 
+    }, []);
+
+    const {
+        Name = '',
+      } = result;
+
     return (
     <div className="content">
         <h2>REPOS HERE</h2>
+        <ul className="list-group">
+        {result.map((result) => (
+        <li className="list-group-item" key={result.name}> name= {result.name}
+        </li>
+      ))}
+    </ul>
     </div>
     );
   };
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const search = async (user) =>
-  axios.get(`https://api.github.com/users/${user}/repos`);
+const search = async (query) =>
+  axios.get(`https://api.github.com/users/${query}/repos`);
+
 
 export default search;
