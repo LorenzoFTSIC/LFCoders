2d44032805a322323a973c5d3b4fd326d24aa9f5
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-06 20:24:11 -0400

    add card template from recent collabs to be a bit uniform. added a style for displaying the cards only when the search is not visible.

--- client/src/pages/Searchcoders.js
+++ client/src/pages/Searchcoders.js
@@ -1,52 +1,44 @@
 import React, { useState } from 'react';
 import robotimg from '../assets/img/robot5.png';
-import { gql, useLazyQuery } from "@apollo/client";
-import { QUERY_PROFILES_BY_SKILL } from '../utils/queries'
-
+import { gql, useLazyQuery } from '@apollo/client';
+import { QUERY_PROFILES_BY_SKILL } from '../utils/queries';
 
 const styles = {
+  checkbox: {
+    backgroundColor: 'pink',
+    borderColor: 'pink'
+  },
+  robotimg: {
+    height: '150px',
+    opacity: '0.8',
+    marginTop: '40px',
+    marginLeft: '50px'
+  },
+  formContainer: {
+    marginTop: '20px'
+  },
 
-    checkbox: {
-      backgroundColor: "pink",
-      borderColor: "pink",
-    },
-    robotimg: {
-        height: "150px",
-        opacity: '0.8',
-        marginTop:'40px',
-        marginLeft: '50px'
-     
-        
-      }, 
-    formContainer: {
-      marginTop: '20px'
-    },
+  modalPage: {
+    marginBottom: '40px'
+  },
 
-    modalPage:{
-        marginBottom: '40px'
-    },
-
-    formContent:{
-        display: "flex",
-        marginLeft:'20px'
-        // aligntItems: "center"
-      
-    }
-  
-  };
-
+  formContent: {
+    display: 'flex',
+    marginLeft: '20px'
+    // aligntItems: "center"
+  }
+};
 
 const Searchcoders = () => {
-
   const [formState, setFormState] = useState({
     errors: {},
     name: '',
     skills: [],
-    status: '',
+    status: ''
   });
 
   // const handleChange = (e) => {
-    
+
   //   const { name, value } = e.target;
   //   console.log(name)
   //   console.log(value)
@@ -66,22 +58,21 @@ const Searchcoders = () => {
     });
   };
 
-  const [queryProfileBySkill, { loading, error, data }] = useLazyQuery(QUERY_PROFILES_BY_SKILL);
+  const [queryProfileBySkill, { loading, error, data }] = useLazyQuery(
+    QUERY_PROFILES_BY_SKILL
+  );
 
   if (loading) return <p>Loading ...</p>;
   if (error) return `Error! ${error}`;
 
   // const projectData = data?.name || [];
 
-  console.log(data)
+  console.log(data);
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-  
 
     queryProfileBySkill({ variables: { skillName: formState.skills } });
-
-  }
-
+  };
 
   return (
     <div className="modalPage" style={styles.modalPage}>
@@ -89,65 +80,121 @@ const Searchcoders = () => {
         {/* <div style={style.container}>
         <h1 className= "title" style={style.title}></h1>
       <div> */}
-        <h3 className="mainTitle" style={styles.mainTitle}>LOOKING FOR CODERS
-      </h3>
-      
-        <form >
+        <h3 className="mainTitle" style={styles.mainTitle}>
+          LOOKING FOR CODERS
+        </h3>
+
+        <form>
           <div style={styles.formContent}>
             <div>
-              <h3 className="sectionHeading">Find someone with these skills:</h3>
-                <div style={styles.formContainer}>
+              <h3 className="sectionHeading">
+                Find someone with these skills:
+              </h3>
+              <div style={styles.formContainer}>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="React" id="react" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="React"
+                    id="react"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="react">
                     React
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="Angular" id="angular" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="Angular"
+                    id="angular"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="angular">
                     Angular
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="MongoDB" id="mongodb" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="MongoDB"
+                    id="mongodb"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="mongodb">
                     MongoDB
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="MySQL" id="mysql" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="MySQL"
+                    id="mysql"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="mysql">
                     MySQL
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="Express" id="express" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="Express"
+                    id="express"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="express">
                     Express
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" style={styles.checkbox} type="checkbox" name="skills" value="Node" id="node" onClick={handleSkills}/>
+                  <input
+                    className="form-check-input"
+                    style={styles.checkbox}
+                    type="checkbox"
+                    name="skills"
+                    value="Node"
+                    id="node"
+                    onClick={handleSkills}
+                  />
                   <label className="form-check-label" htmlFor="node">
                     Nodejs
                   </label>
                 </div>
-              </div> 
+              </div>
             </div>
-            <img className="robotimg" src={robotimg} alt="robot with magnofying glass" style={styles.robotimg}></img>
-            </div>
-            <button
-              className="btn btn-block submit"
-              type="submit"
-              aria-label="Close"
-              onClick={handleFormSubmit}
-            >
-              Submit
-            </button>
-          </form>
-     </div>
-     </div>
+            <img
+              className="robotimg"
+              src={robotimg}
+              alt="robot with magnofying glass"
+              style={styles.robotimg}
+            ></img>
+          </div>
+          <button
+            className="btn btn-block submit"
+            type="submit"
+            aria-label="Close"
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
     //   HTML and CSS by AlvaroTrigo on CodePen: TabbyTabs
   );
 };
